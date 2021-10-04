87f6d47d5c9eef1c7430247698a5d5844e9df805
Dana Sugu <dana.sugu@gmail.com>
2021-10-04 15:37:09 +0300

    update

--- js/dragger.js
+++ js/dragger.js
@@ -2,7 +2,7 @@ const dragit = document.querySelector('#dragger');
 const boxes = document.querySelectorAll('.box');
 
 dragit.addEventListener('dragstart', (e) => {
-  dragit.style.opacity = 0.5;
+  dragit.style.opacity = 0.01;
 });
 dragit.addEventListener('dragend', (e) => {
   dragit.style.opacity = '';
@@ -14,5 +14,13 @@ boxes.forEach((box) => {
   });
   box.addEventListener('dragleave', (e) => {
     e.target.classList.remove('red');
+  });
+  box.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+  box.addEventListener('drop', (e) => {
+    e.preventDefault();
+    console.log('dropped');
+    e.target.appendChild(dragit);
   });
 });
