b085e29160198e36c9a8eee305c485d7914462b8
Dana Sugu <dana.sugu@gmail.com>
2021-10-04 11:12:58 +0300

    Update dragger.js

--- js/dragger.js
+++ js/dragger.js
@@ -2,3 +2,6 @@ const dragit = document.querySelector('#dragger');
 dragit.addEventListener('dragstart', (e) => {
   dragit.style.opacity = 0.5;
 });
+dragit.addEventListener('dragend', (e) => {
+  dragit.style.opacity = '';
+});
